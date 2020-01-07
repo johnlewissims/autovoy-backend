7d678d273aa685edb4c085ba88a25a6f83d45c7a
johnlewissims <johnsimsemail@gmail.com>
2020-01-07 16:31:25 -0500

    Cors Adjustments

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 Route::post('/register', 'AuthController@register');
 Route::post('/login', 'AuthController@login');
-Route::get('/user', 'AuthController@user')->middleware('auth:api');
+Route::get('/user', 'AuthController@user');
 Route::post('/logout', 'AuthController@logout');
 
 Route::post('/logout', 'AuthController@logout');
