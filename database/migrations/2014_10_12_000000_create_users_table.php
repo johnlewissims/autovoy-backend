7fd40f399c94c57ae7fe1e202fba2b1d41eff81e
johnlewissims <johnsimsemail@gmail.com>
2020-03-31 12:39:50 -0400

    Migrations Updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('update_settings');
+            $table->string('zip');
+            $table->string('referral');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
